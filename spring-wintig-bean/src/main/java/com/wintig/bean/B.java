ecbcff29a6762eb9703745879e675049817f2008
wintig <840872473@qq.com>
2020-08-10 04:25:48 +0800

    init

--- spring-wintig-bean/src/main/java/com/wintig/bean/B.java
+++ spring-wintig-bean/src/main/java/com/wintig/bean/B.java
@@ -6,8 +6,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-
 @Component
 public class B implements ApplicationContextAware, InitializingBean {
 
